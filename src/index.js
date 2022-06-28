7da553b32e8f847fb0fd5548072c23f844e11983
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-28 09:06:23 +0200

    Adding assets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../src/pages/App/App";
 import "./styles/main.css";
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>test</React.StrictMode>);
